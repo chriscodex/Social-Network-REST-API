b90e6fde65549dec49811b7ebd459b0a341c3244
Christian <3003html@gmail.com>
2022-07-23 02:30:32 -0500

    Interface server created

--- server/server.go
+++ server/server.go
@@ -1,1 +1,16 @@
 package server
+
+// Items that the server will need to connect
+type Config struct {
+	// Port where it will be executed
+	Port int
+	// Secret key that will be used to generate Tokens
+	JWTSecret string
+	// Database connection
+	DatabaseUrl string
+}
+
+// Interface to be considered a server
+type Server interface {
+	Config() *Config
+}
