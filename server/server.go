84248c5f3bea3389d7f3e2e7b43fd599218dc4f5
Christian <3003html@gmail.com>
2022-07-24 05:31:59 -0500

    Method Start created

--- server/server.go
+++ server/server.go
@@ -3,6 +3,8 @@ package server
 import (
 	"context"
 	"errors"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
@@ -26,7 +28,7 @@ type Server interface {
 type Broker struct {
 	config *Config
 	// It defines the API route
-	router mux.Router
+	router *mux.Router
 }
 
 // Method that makes the broker a server interface
@@ -49,8 +51,18 @@ func NewServer(ctx context.Context, config *Config) (*Broker, error) {
 
 	broker := &Broker{
 		config: config,
-		router: *mux.NewRouter(),
+		router: mux.NewRouter(),
 	}
 
 	return broker, nil
+}
+
+// Method that makes the server Broker able to start
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	b.router = mux.NewRouter()
+	binder(b, b.router)
+	log.Println("Server started on port", b.Config().Port)
+	if err := http.ListenAndServe(b.config.Port, b.router); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
