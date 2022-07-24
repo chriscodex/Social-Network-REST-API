c4997b3b9900d6203a8415354f461f60ad760862
Christian <3003html@gmail.com>
2022-07-24 08:43:16 -0500

    Implementing the repository pattern

---
+++ models/user.go
@@ -0,0 +1,8 @@
+package models
+
+// User model
+type User struct {
+	Id       int64  `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
---
+++ repository/user.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/ChrisCodeX/REST-API-Go/models"
+)
+
+//
+type UserRepository interface {
+	InsertUser(ctx context.Context, user *models.User) error
+	GetUserById(ctx context.Context, id int64) (models.User, error)
+}
+
+var implementation UserRepository
+
+func SetRepository(repository UserRepository) {
+	implementation = repository
+}
+
+func InsertUser(ctx context.Context, user *models.User) error {
+	return implementation.InsertUser(ctx, user)
+}
+
+func GetUserById(ctx context.Context, id int64) (models.User, error) {
+	return implementation.GetUserById(ctx, id)
+}
