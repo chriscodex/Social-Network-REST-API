6568646a29cd307e67d45a54a414705a6bd467d5
Christian <3003html@gmail.com>
2022-07-25 07:08:25 -0500

    SignupHandler code commented

--- handlers/user.go
+++ handlers/user.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/ChrisCodeX/REST-API-Go/models"
@@ -25,30 +26,43 @@ type SignUpResponse struct {
 func SignUpHandler(s server.Server) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var request = SignUpRequest{}
+
+		// Decode the request
 		err := json.NewDecoder(r.Body).Decode(&request)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+
+		// Generate a random id
 		id, err := ksuid.NewRandom()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+
+		// Insert request data in user struct
 		var user = models.User{
 			Email:    request.Email,
 			Password: request.Password,
 			Id:       id.String(),
 		}
+
+		// Send the struct to be stored in the database
 		err = repository.InsertUser(r.Context(), &user)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+
+		// Response to the client
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(SignUpResponse{
 			Id:    user.Id,
 			Email: user.Email,
 		})
+
+		// Message on the server
+		log.Println("User registered successfully")
 	}
 }
