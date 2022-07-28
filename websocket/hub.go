fbc2b399ef6a20040efeb5772edcb50561bfe8de
Christian <3003html@gmail.com>
2022-07-28 11:26:38 -0500

    var upgrader created in hub.go

--- websocket/hub.go
+++ websocket/hub.go
@@ -1,1 +1,12 @@
 package websocket
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	// Restrict access to clients
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
